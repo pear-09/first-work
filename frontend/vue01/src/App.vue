456fe9a5cfc7e901b364c9ce8dff6517333baa9d
gsgqh <128687770+gsgqh@users.noreply.github.com>
2024-11-02 20:13:50 +0800

    1.0 (#20)
    * 将获取记录整合进app.py
    
    * 添加跨域请求
    
    * 修复路由bug
    
    * 增加type
    
    * 1.0
    
    ---------
    
    Co-authored-by: 黎曼 <2901868597@qq.com>

--- frontend/vue01/src/App.vue
+++ frontend/vue01/src/App.vue
@@ -11,7 +11,9 @@
 // eslint-disable-next-line no-unused-vars
 // import { RouterView ,RouterLink} from 'vue-router';
 
+
 import Navy from './components/Navy'
+
 // import ShouYe from './views/ShouYe'
 // import FaBu from './views/FaBu'
 // import DengLu from './views/DengLu'
